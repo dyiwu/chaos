545db161d0a224ddc7b9652f667994a88d5d1d0b
dyiwu <dyiwu.liu@gmail.com>
2020-05-12 07:42:16 +0800

    rebuilding site Tue May 12 07:42:16 CST 2020

--- view.sh
+++ view.sh
@@ -2,3 +2,4 @@
 
 hugo server --disableFastRender
 # hugo --i18n-warnings server
+# hugo server --bind=chaos.example.com --baseURL=http://chaos.example.com:1313
--- view1.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#hugo server --disableFastRender
-# hugo --i18n-warnings server
-hugo server --bind=chaos.example.com --baseURL=http://chaos.example.com:1313
