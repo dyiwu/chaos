a4b097bb023e7fe067faf1d327cf8573cbe3faec
dyiwu <dyiwu.liu@gmail.com>
2021-03-17 20:07:16 +0800

    rebuilding site Wed 17 Mar 2021 08:07:16 PM CST

--- clone_chaos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -e "Clone chaos.git repo from GitHub to local..."
-cd ~/Hugo/Sites
-git clone https://github.com/dyiwu/chaos.git
-cd chaos
--- clone_theme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Hugo/Sites/chaos
-git clone https://github.com/dillonzq/LoveIt.git themes/LoveIt
-
